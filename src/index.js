24a32dcb323da8d571bef8697c06ffcb504c8f0e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-18 11:19:37 +0300

    Added an import for Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(<App />, document.getElementById('root'));
