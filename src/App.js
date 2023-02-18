ca2a2f626e541b0de4219c06f6231ddc6d44f0c4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-18 21:55:31 +0300

    Imported AuthActions from the store

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Auth from './components/Auth';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import UserProfile from './components/UserProfile';
+import { authActions } from './store';
 
 function App()
 {
