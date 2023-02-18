3c26d74d96303222a10338948511d8270a5efb9d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-18 21:56:38 +0300

    Got rid of the AuthActions import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Auth from './components/Auth';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import UserProfile from './components/UserProfile';
-import { authActions } from './store';
 
 function App()
 {
