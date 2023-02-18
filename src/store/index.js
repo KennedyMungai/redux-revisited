4c2865b4ad381bfef5bd34bc55b6d23c137cb297
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-18 12:19:28 +0300

    Added the showCounter item to the state object

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,25 @@
 import { createStore } from 'redux'
 
-const counterReducer = (state = { counter: 0 }, action) => 
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => 
 {
     if (action.type === 'increment')
     {
         return {
-            counter: state.counter + 1
+            counter: state.counter + 1, showCounter: true
         }
     }
 
     if (action.type === 'decrement')
     {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 1, showCounter: true
         }
     }
 
     if (action.type === 'increase')
     {
         return {
-            counter: state.counter + action.amount
+            counter: state.counter + action.amount, showCounter: true
         }
     }
 
