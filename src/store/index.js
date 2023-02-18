ef9400b3b3193c0c9ce07a9e3b97532c45793e7f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-18 12:21:30 +0300

    The counter references state.showCounter

--- src/store/index.js
+++ src/store/index.js
@@ -7,21 +7,24 @@ const counterReducer = (state = initialState, action) =>
     if (action.type === 'increment')
     {
         return {
-            counter: state.counter + 1, showCounter: true
+            counter: state.counter + 1,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === 'decrement')
     {
         return {
-            counter: state.counter - 1, showCounter: true
+            counter: state.counter - 1,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === 'increase')
     {
         return {
-            counter: state.counter + action.amount, showCounter: true
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
         }
     }
 
