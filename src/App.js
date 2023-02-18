50f226a4671d03f3c91c229bf4b6621a0c7649c7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-18 21:54:37 +0300

    Added an import for the user profile component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Auth from './components/Auth';
 import Counter from './components/Counter';
 import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 
 function App()
 {
