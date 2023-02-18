40e5b9f392d5fd8957770dc0e72424f757c2c820
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-18 12:23:36 +0300

    Added an if block for the toggle action type

--- src/store/index.js
+++ src/store/index.js
@@ -28,6 +28,14 @@ const counterReducer = (state = initialState, action) =>
         }
     }
 
+    if (action.type === 'toggle')
+    {
+        return {
+            counter: state.counter,
+            showCounter: !state.showCounter
+        }
+    }
+
     return state
 }
 
